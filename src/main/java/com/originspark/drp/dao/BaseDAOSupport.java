aad19a45269846fe6d285df0fdaa7fb9391897af
yang-xiansen <271819776@qq.com>
2019-02-14 17:47:14 +0800

    修改drp

--- src/main/java/com/originspark/drp/dao/BaseDAOSupport.java
+++ src/main/java/com/originspark/drp/dao/BaseDAOSupport.java
@@ -10,24 +10,29 @@ public abstract class BaseDAOSupport<T> implements BaseDAO<T> {
     @PersistenceContext
     protected EntityManager em;
 
+    @Override
     public void delete(T entity) {
         em.remove(entity);
     }
 
+    @Override
     @Transactional(readOnly = true)
     public T findById(Class<T> c, Long id) {
         return em.find(c, id);
     }
 
+    @Override
     public T save(T entity) {
         em.persist(entity);
         return entity;
     }
 
+    @Override
     public T update(T entity) {
         return em.merge(entity);
     }
 
+    @Override
     @Transactional(readOnly = true)
     public T getReferance(Class<T> c, Long id) {
         return em.getReference(c, id);
